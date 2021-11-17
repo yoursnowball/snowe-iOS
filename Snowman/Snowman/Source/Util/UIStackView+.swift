94c24025a246ff119b4b8a1023d888f427c9c4b2
yoonseo <yo26on@soongsil.ac.kr>
2021-11-18 03:46:53 +0900

    [#10] MoyaLoggingPlugin 추가

--- Snowman/Snowman/Source/Util/UIStackView+.swift
+++ Snowman/Snowman/Source/Util/UIStackView+.swift
@@ -7,8 +7,8 @@
 
 import UIKit.UIStackView
 
-extension UIStackView{
-    func addArrangedSubviews(_ views:UIView...){
+extension UIStackView {
+    func addArrangedSubviews(_ views: UIView...) {
         for view in views {
             self.addArrangedSubview(view)
         }
