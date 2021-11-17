ed4671def1d90573d4b2010ed8b0b57ee1706142
yoonseo <yo26on@soongsil.ac.kr>
2021-11-18 03:38:02 +0900

    [#10] BaseNavigationController, BaseVC 추가

---
+++ Snowman/Snowman/Source/Component/Base/BaseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseViewController.swift
+//  Snowman
+//
+//  Created by 김윤서 on 2021/11/18.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    private func render() {
+        view.backgroundColor = .white
+    }
+}
