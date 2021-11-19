2b970bfacec3122d744936146fd9b55f1f4f50f8
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-11-19 18:43:05 +0900

    [#13] Awards Cell 구현

--- Snowman/Snowman/Source/Feature/Award/View/AwardViewController.swift
+++ Snowman/Snowman/Source/Feature/Award/View/AwardViewController.swift
@@ -9,14 +9,49 @@ import UIKit
 
 final class AwardViewController: BaseViewController {
 
-    private let tableView = UITableView()
+    private lazy var tableView = UITableView().then {
+        $0.registerReusableCell(AwardTableViewCell.self)
+        $0.dataSource = self
+        $0.delegate = self
+        $0.separatorStyle = .none
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLayouts()
+    }
+
+}
+
+extension AwardViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 121
+    }
+}
+
+extension AwardViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: AwardTableViewCell = tableView.dequeueReusableCell(indexPath: indexPath)
+        cell.updateData(
+            with: .red.withAlphaComponent(0.1),
+            goalText: "900점 달성하고 놀러가기",
+            nameText: "수줍은눈사람",
+            characterImageName: "person.fill"
+        )
+        return cell
     }
 
 }
 
 extension AwardViewController {
-    
+    private func setLayouts() {
+        view.addSubviews(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
