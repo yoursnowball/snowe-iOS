45f142e0b36cd7a39f73676ab8e7bc1e68f3a043
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-11-19 11:03:39 +0900

    [#9] 눈덩이 생성 목표 VC 구현

---
+++ Snowman/Snowman/Source/Feature/NewCharacter/GoalQuestionViewController.swift
@@ -0,0 +1,49 @@
+//
+//  GoalQuestionViewController.swift
+//  Snowman
+//
+//  Created by 김윤서 on 2021/11/19.
+//
+
+import UIKit
+
+final class GoalQuestionViewController: QuestionBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setProperties()
+        setDelegation()
+    }
+
+    override func nextButtonDidTapped(_ sender: UIButton) {
+        goToNextViewController()
+    }
+
+}
+
+extension GoalQuestionViewController {
+    private func setDelegation() {
+        textFieldDelegate = self
+    }
+
+    private func setProperties() {
+        numberLabelText = "1/3"
+        questionLabelText = "당신의 목표는 무엇인가요?"
+        descriptionLabelText = "목표"
+        textFieldTextCount = 15
+    }
+
+    private func goToNextViewController() {
+        navigationController?.pushViewController(NameQuestionViewController(), animated: true)
+    }
+}
+extension GoalQuestionViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.returnKeyType == .done {
+            goToNextViewController()
+            return true
+        } else {
+            return false
+        }
+    }
+}
