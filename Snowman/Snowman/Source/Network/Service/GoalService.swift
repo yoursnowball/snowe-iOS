952c474bf1708f9d32f521529ba0a38c2e82a0c8
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-11-20 14:05:40 +0900

    [#18] GoalService 구현

---
+++ Snowman/Snowman/Source/Network/Service/GoalService.swift
@@ -0,0 +1,59 @@
+//
+//  GoalService.swift
+//  Snowman
+//
+//  Created by 김윤서 on 2021/11/20.
+//
+
+import Foundation
+import Moya
+
+public class GoalService {
+    private let goalProvider = MoyaProvider<GoalAPI>(plugins: [MoyaLoggingPlugin()])
+
+    private func postNewGoal(
+        name: String,
+        type: String,
+        objective: String,
+        completion: @escaping (NetworkResult<Any>) -> Void
+    ) {
+        goalProvider.request(.postNewGoal(name: name, type: type, objective: objective)) { (result) in
+            switch result {
+            case.success(let response):
+
+                let statusCode = response.statusCode
+                let data = response.data
+
+                let networkResult = self.judgeStatus(by: statusCode, data)
+                completion(networkResult)
+
+            case .failure(let err):
+                print(err)
+            }
+
+        }
+    }
+
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+
+        let decoder = JSONDecoder()
+
+        switch statusCode {
+        case 200:
+            guard let decodedData = try? decoder.decode(GoalResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .success(decodedData)
+        case 400..<500:
+            guard let decodedData = try? decoder.decode(ErrorResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .requestErr(decodedData.message)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+
+}
