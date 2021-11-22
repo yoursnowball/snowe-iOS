94fce354e75c32f693a6c100e84156b919cfd7ba
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-11-23 02:53:13 +0900

    [#18] Home VC 서버 통신

---
+++ Snowman/Snowman/Source/Util/Notification.Name+.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.Name+.swift
+//  Snowman
+//
+//  Created by 김윤서 on 2021/11/23.
+//
+
+import UIKit
+
+extension Notification.Name {
+    static let refreshHomeVC = Notification.Name("refreshHome")
+}
