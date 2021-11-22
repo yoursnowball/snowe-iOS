18824abca4956e9f85684fabdcfced8fc7da05b2
yoonseo <yo26on@soongsil.ac.kr>
2021-11-22 16:32:23 +0900

    [#14, #18] API Model 수정

--- Snowman/Snowman/Support/AppDelegate.swift
+++ Snowman/Snowman/Support/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         UserDefaults.standard.setValue("eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwidXNlck5hbWUiOiJ0ZXN0IiwiaWF0IjoxNjM3NDI0ODI5LCJleHAiOjE2OTc5MDQ4Mjl9.7U4beCgjbEaBs1unE51teHnUMVX2OuMp0fSGRkZz3xM", forKey: UserDefaultKey.token)
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = BaseNavigationController(rootViewController: HomeViewController())
         window?.backgroundColor = .white
         window?.makeKeyAndVisible()
         return true
