7a6b4ac945636796caf01a9e0a4686078305d036
Yoonseo Kim <yo26on@soongsil.ac.kr>
2021-11-23 04:59:00 +0900

    [#18] Home CollectionView 애니메이션 적용

--- Snowman/Snowman/Resource/Image.swift
+++ Snowman/Snowman/Resource/Image.swift
@@ -13,4 +13,8 @@ enum Image {
     static let todoCountBubble = UIImage(named: "todo_count_bubble")!
     static let checkBold = UIImage(named: "check_bold")!
     static let bubblePolygon = UIImage(named: "bubble_polygon")!
+    static let blueCard = UIImage(named: "180_blue_card_character")!
+    static let greenCard = UIImage(named: "180_green_card_character")!
+    static let orangeCard = UIImage(named: "180_orange_card_character")!
+    static let pinkCard = UIImage(named: "180_pink_card_character")!
 }
