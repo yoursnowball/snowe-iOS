e3fa6c94d0a126c7560d49bc905b16a04ddeccb9
Yoonseo Kim <72497599+ezidayzi@users.noreply.github.com>
2021-11-23 00:14:28 +0900

    [#17] 알림뷰 UI 구현 (#28)
    * [#17] NotificationViewController UI 구현
    
    * [#17] NotificationTableViewCell Layout 구현 완료
    
    * [#17] Cell Layout 수정

---
+++ Snowman/Snowman/Source/Feature/Notification/Cell/NotificationTableViewCell.swift
@@ -0,0 +1,87 @@
+//
+//  NotificationTableViewCell.swift
+//  Snowman
+//
+//  Created by 김윤서 on 2021/11/22.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class NotificationTableViewCell: UITableViewCell {
+
+    private let titleLabel = UILabel().then {
+        $0.font = .spoqa(size: 14, family: .bold)
+    }
+
+    private let contentLabel = UILabel().then {
+        $0.numberOfLines = 0
+        $0.font = .spoqa(size: 14, family: .regular)
+    }
+
+    private let timeLabel = UILabel().then {
+        $0.font = .spoqa(size: 10, family: .regular)
+        $0.textColor = .systemBlue //TODO:- 컬러변경
+    }
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setSelectedBackgroundView()
+        setLayouts()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.text = nil
+        contentLabel.text = nil
+        timeLabel.text = nil
+    }
+
+    private func setSelectedBackgroundView() {
+        selectedBackgroundView = UIView()
+        selectedBackgroundView?.backgroundColor = .blue.withAlphaComponent(0.1)//TODO:- 컬러변경
+    }
+}
+
+extension NotificationTableViewCell {
+    public func updateData(response: NotificationResponse) {
+        titleLabel.text = response.title
+        contentLabel.text = response.content
+        timeLabel.text = response.time
+    }
+}
+
+extension NotificationTableViewCell {
+    private func setLayouts() {
+        contentView.addSubviews(
+            titleLabel,
+            contentLabel,
+            timeLabel
+        )
+
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(56)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.top.equalToSuperview().inset(16)
+        }
+
+        contentLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(56)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(2)
+        }
+
+        timeLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(56)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.top.equalTo(contentLabel.snp.bottom).offset(4)
+            $0.bottom.equalToSuperview().inset(16)
+        }
+    }
+}
---
+++ Snowman/Snowman/Source/Feature/Notification/View/NotificationViewController.swift
@@ -0,0 +1,64 @@
+//
+//  NotificationViewController.swift
+//  Snowman
+//
+//  Created by 김윤서 on 2021/11/22.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class NotificationViewController: BaseViewController {
+
+    private let data: [NotificationResponse] = [
+        NotificationResponse(title: "[눈덩이이름] 진화 완료!", content: "축하합니다🎉 [눈덩이이름]이 진화했어요!", time: "n분 전"),
+        NotificationResponse(title: "[눈덩이이름] 진화 완료!", content: "서두르세요!\n오늘의 투두 완료시간이 얼마 남지 않았어요!", time: "n분 전"),
+        NotificationResponse(title: "[눈덩이이름] 진화 완료!", content: "축하합니다🎉 [눈덩이이름]이 진화했어요!", time: "n분 전")
+    ]
+
+    private lazy var tableView = UITableView().then {
+        $0.delegate = self
+        $0.dataSource = self
+        $0.registerReusableCell(NotificationTableViewCell.self)
+        $0.rowHeight = UITableView.automaticDimension
+        $0.separatorStyle = .none
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Alert"
+        setLayouts()
+    }
+
+}
+
+extension NotificationViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
+extension NotificationViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: NotificationTableViewCell = tableView.dequeueReusableCell(indexPath: indexPath)
+        cell.updateData(response: data[indexPath.row])
+        return cell
+    }
+}
+
+extension NotificationViewController {
+    private func setLayouts() {
+        view.addSubviews(tableView)
+        tableView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalToSuperview()
+        }
+    }
+}
