ad3a8aeee9d88ca8f7b78acc0aef1b55933598b2
Yonghyun Kim <lygon55555@gmail.com>
2021-11-24 01:18:12 +0900

    [#31] String 익스텐션 추가

---
+++ Snowman/Snowman/Source/Util/String+.swift
@@ -0,0 +1,18 @@
+//
+//  String+.swift
+//  Snowman
+//
+//  Created by Yonghyun on 2021/11/24.
+//
+
+import Foundation
+
+extension String {
+    func toDate() -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        
+        return dateFormatter.date(from: self)!
+    }
+}
