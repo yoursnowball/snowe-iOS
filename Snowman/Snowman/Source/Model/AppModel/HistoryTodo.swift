2105ce80fb7aefeb5dde2342a3d033de78d979ed
Yonghyun Kim <lygon55555@gmail.com>
2021-11-24 20:55:50 +0900

    [#31] HistoryTodo 추가

---
+++ Snowman/Snowman/Source/Model/AppModel/HistoryTodo.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryTodo.swift
+//  Snowman
+//
+//  Created by Yonghyun on 2021/11/23.
+//
+
+import Foundation
+
+struct HistoryTodoGroup {
+    var type: Snowe
+    var title: String?
+    var date: String
+    var todoTotalCount: String?
+    var historyTodos: [HistoryTodo]
+}
+
+struct HistoryTodo {
+    var name: String
+    var succeed: Bool
+}
