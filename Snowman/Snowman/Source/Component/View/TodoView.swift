f7fa32a4a840a8f1d3e4d42628bef897157dbcc6
Yonghyun Kim <lygon55555@gmail.com>
2021-11-28 06:09:57 +0900

    [#32] TodoView 추가

---
+++ Snowman/Snowman/Source/Component/View/TodoView.swift
@@ -0,0 +1,14 @@
+//
+//  TodoView.swift
+//  Snowman
+//
+//  Created by Yonghyun on 2021/11/28.
+//
+
+import UIKit
+
+class TodoView: UIView {
+    var todoId: Int!
+    var name: String!
+    var succeed: Bool!
+}
