7c7c4047dc835538d536c688c7b313cfab1e8f7c
Yonghyun Kim <lygon55555@gmail.com>
2021-11-29 23:56:50 +0900

    [#55] 날짜 익스텐션 추가

--- Snowman/Snowman/Source/Util/Date+.swift
+++ Snowman/Snowman/Source/Util/Date+.swift
@@ -40,4 +40,12 @@ extension Date {
         let weekRange = calendar.range(of: .weekOfMonth, in: .month, for: date)
         return weekRange!.count
    }
+
+    func startOfMonth() -> Date {
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self)))!
+    }
+
+    func endOfMonth() -> Date {
+        return Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: self.startOfMonth())!
+    }
 }
