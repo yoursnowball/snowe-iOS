b66abdbcae0e638e99a90bd504b083c20ab2bdc8
Yonghyun Kim <lygon55555@gmail.com>
2021-11-30 00:28:57 +0900

    [#64] LevelChange 열거형 추가

---
+++ Snowman/Snowman/Source/Model/AppModel/LevelChange.swift
@@ -0,0 +1,12 @@
+//
+//  LevelChange.swift
+//  Snowman
+//
+//  Created by Yonghyun on 2021/11/30.
+//
+
+enum LevelChange: String {
+    case keep = "KEEP"
+    case levelDown = "LEVELDOWN"
+    case levelUp = "LEVELUP"
+}
